3058a72eff48be2f901b1d423b2691610c163b7f
Swhag <khg5293@outlook.com>
2023-01-16 23:41:11 -0500

    Update ExperienceOut.js

--- src/components/CVPreview/ExperienceOut.js
+++ src/components/CVPreview/ExperienceOut.js
@@ -14,7 +14,8 @@ function ExperienceOut(props) {
           <div className='experience' key={exp.id}>
             <div className='experience-heading'>
               <h4>{exp.company}</h4>
-              <span className='italic'> | {exp.location}</span>
+              <span>|</span>
+              <span className='italic'>{exp.location}</span>
             </div>
             <div className='experience-info'>
               <div>{exp.position}</div>
